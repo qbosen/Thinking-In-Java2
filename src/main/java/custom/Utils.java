0c387d8cbb375a89cb46e0941b9e72038b25d2ec
qiubaisen <abosen@qq.com>
2018-11-22 20:05:19 +0800

    io

--- src/main/java/custom/Utils.java
+++ src/main/java/custom/Utils.java
@@ -1,5 +1,7 @@
 package custom;
 
+import org.junit.Test;
+
 /**
  * @author qiubaisen
  * @date 2018/11/21
@@ -7,5 +9,19 @@ package custom;
 public class Utils {
     public static <T> T[] toArr(T... ts) {
         return ts;
+    }
+
+    public static String relativeFilePath(Class clazz) {
+        return relativeFilePath(clazz, "java");
+    }
+
+
+    public static String relativeFilePath(Class clazz, String ext) {
+        return String.format("src/main/java/%s/%s.%s", clazz.getPackage().getName(), clazz.getSimpleName(), ext);
+    }
+
+    @Test
+    public void test() {
+        System.out.println(relativeFilePath(getClass()));
     }
 }
--- src/main/java/io/TestEOF.java
+++ src/main/java/io/TestEOF.java
@@ -1,15 +1,37 @@
 //: io/TestEOF.java
 package io; /* Added by Eclipse.py */
 // Testing for end of file while reading a byte at a time.
-import java.io.*;
+
+import custom.Utils;
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class TestEOF {
-  public static void main(String[] args)
-  throws IOException {
-    DataInputStream in = new DataInputStream(
-      new BufferedInputStream(
-        new FileInputStream("TestEOF.java")));
-    while(in.available() != 0)
-      System.out.print((char)in.readByte());
-  }
+    public static void main(String[] args)
+            throws IOException {
+        DataInputStream in = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(Utils.relativeFilePath(TestEOF.class))));
+        while (in.available() != 0)
+            System.out.print((char) in.readByte());
+    }
+
+
+    @Test
+    public void test() throws Exception {
+        byte[] buf = new byte[512];
+        DataInputStream inputStream = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(Utils.relativeFilePath(getClass()))));
+
+        while (inputStream.available() != 0) {
+            int read = inputStream.read(buf);
+            System.out.print(new String(buf, 0, read));
+        }
+        inputStream.close();
+    }
 } /* (Execute to see output) *///:~
