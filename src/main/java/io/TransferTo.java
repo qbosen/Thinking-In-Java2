06c96f80b2167a033c908f8b97f1401aa279dcb2
qiubaisen <abosen@qq.com>
2018-12-03 16:19:33 +0800

    nio channel 复制文件

--- src/main/java/io/TransferTo.java
+++ src/main/java/io/TransferTo.java
@@ -2,20 +2,31 @@
 package io; /* Added by Eclipse.py */
 // Using transferTo() between channels
 // {Args: TransferTo.java TransferTo.txt}
-import java.nio.channels.*;
-import java.io.*;
 
+import custom.Utils;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+@SuppressWarnings("Duplicates")
 public class TransferTo {
-  public static void main(String[] args) throws Exception {
-    if(args.length != 2) {
-      System.out.println("arguments: sourcefile destfile");
-      System.exit(1);
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("arguments: sourcefile destfile");
+            System.exit(1);
+        }
+        FileChannel
+                in = new FileInputStream(args[0]).getChannel(),
+                out = new FileOutputStream(args[1]).getChannel();
+        in.transferTo(0, in.size(), out);
+        // Or:
+        // out.transferFrom(in, 0, in.size());
     }
-    FileChannel
-      in = new FileInputStream(args[0]).getChannel(),
-      out = new FileOutputStream(args[1]).getChannel();
-    in.transferTo(0, in.size(), out);
-    // Or:
-    // out.transferFrom(in, 0, in.size());
-  }
+
+    @Test
+    public void testMain() throws Exception {
+        TransferTo.main(Utils.toArr(Utils.relativeFilePath(TransferTo.class), TransferTo.class.getSimpleName() + ".out"));
+    }
 } ///:~
